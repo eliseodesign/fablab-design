8589c6855930ff0fd90c9eb007030eff81b53ff6
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-09-04 20:22:10 -0600

    :tada: initial commit

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector(".hamburger");
+const modal = document.querySelector(".bg-modal");
+const navbar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger-active")
+  modal.classList.toggle("bg-modal-active")
+  navbar.classList.toggle("navbar-active")
+})
